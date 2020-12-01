ec147ece118bb68761f3b07713c37b5843dd4528
Pawel Bak <pawelbak300589@gmail.com>
2020-12-01 19:22:11 +0000

    Create auth service functions

--- api/src/services/auth.js
+++ api/src/services/auth.js
@@ -1,6 +1,19 @@
+import { Op } from 'sequelize';
 import config from '../config/app.json';
 import { User } from '../models';
-import { generateJwtToken, generateRefreshToken } from '../helpers';
+import {
+  Role,
+  generateJwtToken,
+  generateRefreshToken,
+  getRefreshToken,
+  randomTokenString,
+  hash,
+} from '../helpers';
+import {
+  sendAlreadyRegisteredEmail,
+  sendPasswordResetEmail,
+  sendVerificationEmail,
+} from '../emails';
 
 const authenticate = async ({ email, password, ipAddress }) => {
   const user = User.scope('withHash').findOne({
@@ -30,32 +43,111 @@ const authenticate = async ({ email, password, ipAddress }) => {
   };
 };
 
-const refreshToken = async () => {
-  // TODO:
+const refreshToken = async ({ token, ipAddress }) => {
+  const refreshToken = await getRefreshToken(token);
+  const user = await refreshToken.getUser();
+
+  // replace old refresh token with a new one and save
+  const newRefreshToken = generateRefreshToken(user, ipAddress);
+  refreshToken.revoked = Date.now();
+  refreshToken.revokedByIp = ipAddress;
+  refreshToken.replacedByToken = newRefreshToken.token;
+  await refreshToken.save();
+  await newRefreshToken.save();
+
+  // generate new jwt
+  const jwtToken = generateJwtToken(user);
+
+  // return basic details and tokens
+  return {
+    ...basicDetails(user),
+    jwtToken,
+    refreshToken: newRefreshToken.token,
+  };
 };
 
-const revokeToken = async () => {
-  // TODO:
+const revokeToken = async ({ token, ipAddress }) => {
+  const refreshToken = await getRefreshToken(token);
+
+  // revoke token and save
+  refreshToken.revoked = Date.now();
+  refreshToken.revokedByIp = ipAddress;
+  await refreshToken.save();
 };
 
-const register = async () => {
-  // TODO:
+const register = async (params, origin) => {
+  // validate
+  if (await User.findOne({ where: { email: params.email } })) {
+    // send already registered error in email to prevent user enumeration
+    return await sendAlreadyRegisteredEmail(params.email, origin);
+  }
+
+  // create user object
+  const user = new User(params);
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.count()) === 0;
+  user.role = isFirstAccount ? Role.Admin : Role.User;
+  user.verificationToken = randomTokenString();
+
+  // hash password
+  user.passwordHash = await hash(params.password);
+
+  // save user
+  await user.save();
+
+  // send email
+  await sendVerificationEmail(user, origin);
 };
 
-const verifyEmail = async () => {
-  // TODO:
+const verifyEmail = async ({ token }) => {
+  const user = await User.findOne({
+    where: { verificationToken: token },
+  });
+
+  if (!user) throw Error('Verification failed');
+
+  user.verified = Date.now();
+  user.verificationToken = null;
+  await user.save();
 };
 
-const forgotPassword = async () => {
-  // TODO:
+const forgotPassword = async ({ email }, origin) => {
+  const user = await User.findOne({ where: { email } });
+
+  // always return ok response to prevent email enumeration
+  if (!user) return;
+
+  // create reset token that expires after 24 hours
+  user.resetToken = randomTokenString();
+  user.resetTokenExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  await user.save();
+
+  // send email
+  await sendPasswordResetEmail(user, origin);
 };
 
-const validateResetToken = async () => {
-  // TODO:
+const validateResetToken = async ({ token }) => {
+  const user = await User.findOne({
+    where: {
+      resetToken: token,
+      resetTokenExpires: { [Op.gt]: Date.now() },
+    },
+  });
+
+  if (!user) throw Error('Invalid token');
+
+  return user;
 };
 
-const resetPassword = async () => {
-  // TODO:
+const resetPassword = async ({ token, password }) => {
+  const user = await validateResetToken({ token });
+
+  // update password and remove reset token
+  user.passwordHash = await hash(password);
+  user.passwordReset = Date.now();
+  user.resetToken = null;
+  await user.save();
 };
 
 export default {
