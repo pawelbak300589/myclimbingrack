6a0b7a2e96cd749537449a8a04032087f459d2b3
Pawel Bak <pawelbak300589@gmail.com>
2021-01-23 13:46:12 +0000

    Add verify email page

---
+++ client/src/pages/user/verify-email/verify-email.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import RegisterForm from "../../components/forms/register-form/register-form.component";
+
+// import './verify-email.styles.css';
+
+const VerifyEmailPage = () => {
+    // TODO: create layouts for GuestPages UserPages AuthPages  https://medium.com/@benkissi/creating-multiple-layouts-in-react-react-router-v5-cebde25ff6e6
+    return (
+        <div className="verify-email-page">
+            verify-email Page
+        </div>
+    );
+};
+
+export default VerifyEmailPage;
