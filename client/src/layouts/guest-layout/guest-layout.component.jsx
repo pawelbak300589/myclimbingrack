1c0997a7389aea4101bace5e631f553239a1fce6
Pawel Bak <pawelbak300589@gmail.com>
2021-01-28 10:08:58 +0000

    Update code after removing old headers

--- client/src/layouts/guest-layout/guest-layout.component.jsx
+++ client/src/layouts/guest-layout/guest-layout.component.jsx
@@ -3,7 +3,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-import GuestHeader from "../../components/header/guest-header.component";
 import Header from "../../components/header/header.component";
 
 
--- client/src/layouts/user-layout/user-layout.component.jsx
+++ client/src/layouts/user-layout/user-layout.component.jsx
@@ -3,7 +3,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-import UserHeader from "../../components/header/user-header.component";
 import Header from "../../components/header/header.component";
 
 const UserLayout = props => {
